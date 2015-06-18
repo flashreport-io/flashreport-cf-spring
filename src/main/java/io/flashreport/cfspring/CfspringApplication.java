2cbfcb94a82f2341c68ffb173590aca11c6229c8
nlejeune <nlejeune@flashreport.io>
2015-06-18 17:07:28 +0200

    Add Thymeleaf dependency, a home page, and the report request created page

--- src/main/java/io/flashreport/cfspring/CfspringApplication.java
+++ src/main/java/io/flashreport/cfspring/CfspringApplication.java
@@ -2,11 +2,19 @@ package io.flashreport.cfspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class CfspringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CfspringApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
     }
 }
--- src/main/java/io/flashreport/cfspring/controller/ReportController.java
+++ src/main/java/io/flashreport/cfspring/controller/ReportController.java
@@ -3,17 +3,19 @@ package io.flashreport.cfspring.controller;
 import io.flashreport.cfspring.integration.ReportStatus;
 import io.flashreport.cfspring.service.ReportManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.util.UriComponents;
 
 /**
  * Created by Nicolas Lejeune on 16/06/15.
  */
-@RestController
+@Controller
 @SuppressWarnings("unused")
 public class ReportController {
 
@@ -21,17 +23,21 @@ public class ReportController {
     ReportManager reportManager;
 
     @RequestMapping(value = "/report/new", method = {RequestMethod.GET, RequestMethod.POST})
-    public String requestReportGeneration() {
+    public String requestReportGeneration(Model model) {
         String uuid = reportManager.generateReport();
 
         UriComponents uriComponents = MvcUriComponentsBuilder
                 .fromMethodName(ReportController.class, "viewReportStatus", uuid).build();
 
-        return "Report request sent, check it at " + uriComponents.encode().toUri().toString();
+        model.addAttribute("REPORT_UUID", uuid);
+        model.addAttribute("REPORT_STATUS_URL", uriComponents.encode().toUri().toString());
+        model.addAttribute("REPORT_DOWNLOAD_URL", uriComponents.encode().toUri().toString() + "/download");
 
+        return "created";
     }
 
     @RequestMapping("/report/{uuid}")
+    @ResponseBody
     public ReportStatus viewReportStatus(@PathVariable String uuid) {
         return reportManager.getReportStatus(uuid);
     }
